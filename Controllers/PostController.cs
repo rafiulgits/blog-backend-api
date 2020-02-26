fd2b28513402d1830d7968ce46fc74a9f835f42f
Rafiul Islam <avoidcloud@gmail.com>
2020-02-26 11:42:58 +0600

    endpoints to fetch post objects
    individual, all, pagination fetch support

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -28,6 +29,38 @@ namespace Blogger.Controllers
                 return Created(result.Id.ToString(), result);
             }
             return BadRequest(post.Errors);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Post>> GetPost(Guid id)
+        {
+            var result = await postService.Get(id);
+            if(result == null)
+            {
+                return NotFound();
+            }
+            return result;
+        }
+
+        [HttpGet("page/{number}")]
+        public async Task<ActionResult<List<Post>>> GetPaginate(int number)
+        {
+            var queryOrder = Request.Query["order"].ToString();
+            if(!String.IsNullOrEmpty(queryOrder))
+            {
+                queryOrder = queryOrder.ToLower();
+                if(queryOrder == "desc")
+                {
+                    return await postService.GetPage(number, 10 ,true);
+                }
+            }
+            return await postService.GetPage(number);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Post>>> GetAllPosts()
+        {
+            return await postService.GetAll();
         }
     }
 }
